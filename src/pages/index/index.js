24c92d22c2090f23738776c657cc8fadb249a9f2
yiermi <1023402488@qq.com>
2019-05-26 00:09:31 +0800

    1

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text, Button } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import { AtButton, AtTabBar } from 'taro-ui'
 import { connect } from '@tarojs/redux'
 
 import { add, minus, asyncAdd } from '../../actions/counter'
@@ -23,6 +23,15 @@ import './index.scss'
 }))
 class Index extends Component {
 
+  state = {
+    current: null,
+    tabList: [
+      { title: '首页', iconType: 'home', text: 'new' },
+      { title: '信息专栏', iconType: 'message', text: '100', max: '99' },
+      { title: '个人中心', iconType: 'user' }
+    ]
+  }
+
   config = {
     navigationBarTitleText: '首页'
   }
@@ -37,6 +46,12 @@ class Index extends Component {
 
   componentDidHide() { }
 
+  handleClick = (value) => {
+    this.setState({
+      current: value
+    })
+  }
+
   render() {
     return (
       <View className='index'>
@@ -46,6 +61,14 @@ class Index extends Component {
         <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
         <View><Text>{this.props.counter.num}</Text></View>
         <View><Text>Hello, World</Text></View>
+        <View>
+          <AtTabBar
+            fixed
+            tabList={this.state.tabList}
+            onClick={this.handleClick.bind(this)}
+            current={this.state.current}
+          />
+        </View>
       </View>
     )
   }
